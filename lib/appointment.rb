130118eecaaa2735abbe0c6f1b1738341977474c
github username <github email address>
2017-04-09 15:56:31 -0400

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,25 @@
+# class Appointment
+#
+#   attr_accessor :date, :doctor, :patient
+#
+#   def initialize(date, doctor)
+#
+#     @date = date
+#     @doctor = doctor
+#     @patient = patient
+#     doctor.add_appointment(self)
+#
+#   end
+# end
+
+
+class Appointment
+  attr_accessor :date, :doctor, :patient
+
+  def initialize(date, doctor)
+    @date = date
+    @doctor = doctor
+    doctor.add_appointment(self)
+  end
+
+end
